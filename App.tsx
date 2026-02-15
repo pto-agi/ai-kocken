6cd4722c8c98f7895ea9ef83933ee3a9828a7f13
Marcus <marcus@Marcus-MacBook-Air.local>
2026-02-15 19:56:32 +0100

    Use browser router and add Vercel SPA rewrites

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect } from 'react';
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { useAuthStore } from './store/authStore';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
